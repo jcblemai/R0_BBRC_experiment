b8a68edc0e8de1959005a7fa60b1452382918d06
javierps <javier.perezsaez@gmail.com>
2020-04-03 14:55:58 +0200

    first pomp commit

---
+++ COVID-pomp/scripts/setup_pomp.R
@@ -0,0 +1,117 @@
+
+# Preamble ---------------------------------------------------------------------
+
+library(tidyverse)
+library(magrittr)
+library(foreach)
+library(iterators)
+source("scripts/utils.R")
+
+# Load hospitalization data ----------------------------------------------------
+# Date to replace on the right bound
+right_date <- as.Date("2020-04-01")
+
+# Hospital data
+hosp_data <- read_csv("../COVID-19_CHVaud/analysis_CH/data/VD/hospitalization_data_full.csv") %>% 
+  mutate_at(c("date_in", "date_out", "icu_in", "icu_out"), 
+            function(x) as.Date(x, format = "%m/%d/%Y")) %>%  
+  filter(date_in > "2020-02-28") %>% 
+  mutate(deceased = as.logical(deceased),
+         outcome = case_when(deceased ~ "deceased",
+                             !is.na(date_out) ~ "discharged",
+                             T ~ "hospitalized"), 
+         date_out = case_when(is.na(date_out) ~ right_date,
+                              T ~ date_out),
+         time_hosp = difftime(date_out, date_in, units = "days"),
+         icu_state = case_when(!is.na(icu_in) & !is.na(icu_out) ~ "discharged from ICU",
+                               !is.na(icu_in) & is.na(icu_out) ~ "in ICU",
+                               T ~ ""),
+         icu_out = case_when(!is.na(icu_in) & is.na(icu_out) ~ right_date,
+                             !is.na(icu_out) ~ icu_out),
+         time_icu = difftime(icu_out, icu_in, units = "days"),
+         time_to_death = difftime(date_out, date_in, units = "days"),
+         time_to_icu= difftime(icu_in, date_in, units = "days"),
+         time_icu_to_death = difftime(date_out, icu_in, units = "days")) %>% 
+  mutate_at(vars(contains("time_")), as.numeric)
+
+
+hosp_cumul <-  bind_rows(
+  lapply(seq.Date(min(hosp_data$date_in), max(hosp_data$date_out), by = "1 days"),
+         function(x) {
+           with(hosp_data,
+                tibble(
+                  date = x,
+                  hosp_incid = sum(date_in == x),
+                  icu_incid = sum(icu_in == x, na.rm = T),
+                  deaths_icu_incid = sum(date_out == x & !is.na(icu_in) & outcome == "deceased"),
+                  deaths_noicu_incid = sum(date_out == x & is.na(icu_in) & outcome == "deceased"),
+                  hosp_curr = sum(date_in <= x & (date_out > x | (date_out ==x & outcome == "hospitalized"))),
+                  icu_curr = sum(icu_in <= x & (icu_out > x |(icu_out == x & outcome == "hospitalized")), na.rm = T)
+                )
+           )
+         }))
+
+write_csv(hosp_cumul, path = "data/VD_hosp_data.csv")
+hosp_cumul %>% 
+  gather(var, value, -date) %>% 
+  ggplot(aes(x = date, y = value, color = var)) +
+  geom_line() +
+  facet_wrap(~ var, scales = "free_y")
+# Times ------------------------------------------------------------------------
+
+
+
+obs_times <- list(
+  h2icu = filter(hosp_data, !is.na(time_to_icu), icu_state != "")[["time_to_icu"]],
+  icu2d = filter(hosp_data, outcome == "deceased", !is.na(icu_in))[["time_icu_to_death"]],
+  icu2r = filter(hosp_data, outcome != "deceased", !is.na(icu_in))[["time_icu"]],
+  h2d_noicu = filter(hosp_data, outcome == "deceased", is.na(icu_in))[["time_to_death"]],
+  h2r = filter(hosp_data, outcome != "deceased", is.na(icu_in))[["time_hosp"]]
+)
+
+# Number of compartments
+num_comp <- bind_rows(
+  mapply(function(x, t) {mutate(fitErland(x, zero_replace = .75), t = t)},
+         x = obs_times,
+         t = names(obs_times),
+         SIMPLIFY = F)
+)
+
+# Plot to check
+p_erlangs  <- foreach(erlang = iter(num_comp, by = "row"),
+                      obs = obs_times,
+                      .combine = rbind) %do% {
+                        tibble(var = erlang$t, 
+                               value = obs, 
+                               density = NA,
+                               type = "obs") %>% 
+                          rbind(tibble(var = erlang$t, 
+                                       value = seq(0.1, 30, by = .1),
+                                       type = "density") %>% 
+                                  mutate(density = dgamma(value, shape = erlang$k, rate = erlang$lambda)))
+                      } %>% 
+  {
+    ggplot() +
+      geom_histogram(data = filter(., type == "obs"), aes(x = value, y = ..density..), fill = "gray") +
+      geom_line(data = filter(., type == "density"), aes(x = value, y = density)) +
+      geom_text(data = mutate(num_comp, var = t, label = str_c("k = ", k)), aes(x = 25, y = .35, label = label)) +
+      facet_wrap(~var) +
+      theme_bw()
+  }
+
+plot(p_erlangs)
+
+
+# Compute probabilities --------------------------------------------------------
+hosp_known <- filter(hosp_data, outcome != "hospitalized") %>% 
+  mutate(icu = !is.na(icu_in))
+
+n_known <- nrow(hosp_known)
+
+pi2hs <- with(hosp_known, sum(outcome == "discharged" & !icu)/n_known)
+ph2u <- with(hosp_known, sum(icu)/(sum(icu) + sum(!icu & deceased)))
+pu2d <- with(hosp_known, sum(icu & deceased)/sum(icu))
+
+
+
+
---
+++ COVID-pomp/scripts/utils.R
@@ -0,0 +1,38 @@
+erlangLL <- function(lambda, x, k) {
+  -1 * sum(dgamma(x, shape = k , rate = lambda, log = T))
+}
+
+fitErland <- function(x, ks = 1:10, zero_replace = .5) {
+  x[x == 0] <- zero_replace
+  bind_rows(
+    lapply(ks, 
+           function(k) 
+             optimize(erlangLL, x = x, k = k, interval = c(0, 1)) %>% 
+             as_tibble() %>% 
+             mutate(k = k, 
+                    ll = -1 * objective,
+                    lambda = minimum,
+                    mean = k/lambda,
+                    variance = k/(lambda^2)) %>%
+             select(k, ll, lambda, mean, variance)
+             
+    )
+  ) %>% 
+    arrange(desc(ll)) %>% 
+    slice(1)
+}
+
+
+
+# function to draw random vriables from a Drichillet distribution using independent gammas
+rdirichlet <- function (n, alpha) {
+  if (!is.null(salpha <- dim(alpha))) {
+    x <- matrix(rgamma(prod(salpha) * n, rep(as.vector(as.matrix(alpha)), each = n)), ncol = salpha[2], byrow = F)
+    sm <- x %*% rep(1, salpha[2])
+  } else {
+    l <- length(alpha)
+    x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
+    sm <- x %*% rep(1, l)
+  }
+  return(x/as.vector(sm))
+}
