1c4372d7230967f23c65f8d2ad4cabd55d44a260
javierps <javier.perezsaez@gmail.com>
2020-04-06 19:55:30 +0200

    figures report

--- COVID-pomp/scripts/postprocess_results.R
+++ COVID-pomp/scripts/postprocess_results.R
@@ -11,7 +11,10 @@ source("COVID-pomp/scripts/graph_utils.R")
 
 cantons_to_analyze <- c("AG", "BE", "FR", "GE", "NE", "VD", "VS", "ZH")
 
-
+# Time windows over which to compute changes in R
+tw_left <- as.Date(c("2020-03-01", "2020-03-10"))
+tw_right <- as.Date(c("2020-03-29", "2020-04-03"))
+                   
 # Load results -----------------------------------------------------------------
 # Load data that has been produced
 geodata <- read.csv("data/ch/geodata.csv")
@@ -34,8 +37,11 @@ states_to_plot <- c("tot_I", "Rt", "D", "H_curr", "U_curr")
 sims <- getStates(ffilter, states_to_plot)
 # Extract statistics
 filterstats <- computeFilterStats(sims)
-r0_reduction <- computeR0Reduction(filter(sims, var == "Rt"), 7, 7)
+r0_reduction <- computeR0Reduction(filter(sims, var == "Rt"), 
+                                   tw_left = tw_left, 
+                                   tw_right = tw_right,
+                                   date_start = "2020-03-15")
 
 saveRDS(filterstats, file = "COVID-pomp/results/filtered_states_all.rds")
 saveRDS(r0_reduction, file = "COVID-pomp/results/R0_reduction.rds")
-
+save(tw_left, tw_right, file = "COVID-pomp/results/computation_time_windows.rda")
