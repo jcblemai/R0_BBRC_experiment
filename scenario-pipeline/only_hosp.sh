9f397ea90e56a2c8c5ecf0220bb59cef7fb589ce
chadi <joseph.lemaitre@epfl.ch>
2020-04-10 21:44:37 +0200

    iploaded

--- scenario-pipeline/only_hosp.sh
+++ scenario-pipeline/only_hosp.sh
@@ -4,9 +4,9 @@ rm -Rf hospitalization/
 Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Current -d low -j 16 -c config.yml
 Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Current -d med -j 16 -c config.yml
 Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Current -d high -j 16 -c config.yml
-#Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Stopped -d low -j 16 -c config.yml
-#Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Stopped -d med -j 16 -c config.yml
-#Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Stopped -d high -j 16 -c config.yml
+Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Stopped -d low -j 16 -c config.yml
+Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Stopped -d med -j 16 -c config.yml
+Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s Stopped -d high -j 16 -c config.yml
 Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s TestIsolate1 -d low -j 16 -c config.yml
 Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s TestIsolate1 -d med -j 16 -c config.yml
 Rscript COVIDScenarioPipeline/R/scripts/hosp_run.R -s TestIsolate1 -d high -j 16 -c config.yml
