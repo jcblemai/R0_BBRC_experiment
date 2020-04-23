56e63954269c2b41cbb45a0febe4c11010ee1e4e
javierps <javier.perezsaez@gmail.com>
2020-04-23 08:22:54 +0200

    runall

--- run-all_pomp.sh
+++ run-all_pomp.sh
@@ -3,13 +3,13 @@
 # We assume running this from the script directory
 lls=('d-deltah')
 #lls=('d')
-#places=('CH')
-places=('FR' 'GE' 'GR' 'JU' 'NE' 'TI' 'VD' 'VS' 'ZH' 'BS' 'BL')
+places=('CH')
+#places=('FR' 'GE' 'GR' 'JU' 'NE' 'TI' 'VD' 'VS' 'ZH' 'BS' 'BL')
 # missing: AG AI AR GL NW OW SG SH SO SZ TG UR ZG "LU"
 #'BE' 'BL' 'BS' 
 for place in ${places[@]}; do
   for ll in ${lls[@]}; do
-    Rscript COVID-pomp/scripts/build_canton_pomp.R -p ${place} -l ${ll} -r 3 -o 8 -j 8
+    Rscript COVID-pomp/scripts/build_canton_pomp.R -p ${place} -l ${ll} -r 3 -o 8 -j 16
     Rscript COVID-pomp/scripts/filter_canton_pomp.R -p ${place} -l ${ll} -o 8 -n 1000 
   done
 done
