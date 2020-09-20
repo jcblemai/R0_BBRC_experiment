2c9c6c9487d3255b877e7b5f732d7bffffb1e24d
Chadi <joseph.lemaitre@epfl.ch>
2020-09-20 11:56:12 +0200

    ready for reproduce runs ?

--- run-all_pomp.sh
+++ run-all_pomp.sh
@@ -8,14 +8,14 @@ lls=('d-deltah')
 #places=('FR' 'GE' 'GR' 'JU' 'NE' 'TI' 'VD' 'VS' 'ZH' 'BS' 'BL')
 #places=('AG' 'AI' 'AR' 'GL' 'NW' 'OW' 'SG' 'SH' 'SO' 'SZ' 'TG' 'UR' 'ZG' 'LU' 'BE')
 #places=("BE" "BL" "BS" "FR" "GE" "GR" "JU" "NE" "TI" "VS" "VD" "ZH")
-places=("BE" "BL" "BS" "FR" "GE")
+places=("CH")
 # missing: AG AI AR GL NW OW SG SH SO SZ TG UR ZG "LU"
 #'BE' 'BL' 'BS'
 
 for place in ${places[@]}; do
   for ll in ${lls[@]}; do
-    Rscript COVID-pomp/scripts/build_canton_pomp.R -p ${place} -l ${ll} -r 3 -o 8 -j 16
-    Rscript COVID-pomp/scripts/filter_canton_pomp.R -p ${place} -l ${ll} -o 8 -n 1000
-    Rscript COVID-pomp/scripts/plot_filter_canton_pomp.R -p ${place} -l ${ll}
+    Rscript COVID-pomp/scripts/build_canton_pomp.R -p ${place} -l ${ll} -r 3 -o 8 -j 16 -s gamma3
+    Rscript COVID-pomp/scripts/filter_canton_pomp.R -p ${place} -l ${ll} -o 8 -n 1000 -s gamma3
+    Rscript COVID-pomp/scripts/plot_filter_canton_pomp.R -p ${place} -l ${ll} -s gamma3
   done
 done
